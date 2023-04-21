7edb66f873224e1b2477f04c6c3f4a14b4c47371
Schlagonia <schlagonia@gmail.com>
2023-04-21 10:46:00 -0600

    build: vault apr

--- contracts/AprOracle/AprOracle.sol
+++ contracts/AprOracle/AprOracle.sol
@@ -3,6 +3,14 @@ pragma solidity 0.8.18;
 
 import {IStrategy} from "../interfaces/IStrategy.sol";
 
+interface IVault {
+    function totalAssets() external view returns (uint256);
+
+    function profitUnlockingRate() external view returns (uint256);
+
+    function fullProfitUnlockDate() external view returns (uint256);
+}
+
 interface IOracle {
     function aprAfterDebtChange(
         address _asset,
@@ -12,6 +20,9 @@ interface IOracle {
 
 contract AprOacle {
     mapping(address => address) public oracles;
+
+    uint256 internal constant MAX_BPS_EXTENDED = 1_000_000_000_000;
+    uint256 internal constant SECONDS_PER_YEAR = 31_556_952;
 
     function getExpectedApr(
         address _strategy,
@@ -34,5 +45,28 @@ contract AprOacle {
         oracles[_strategy] = _oracle;
     }
 
-    // TODO: Add vault apr calculation
+    /**
+     * @notice Get the expected APR for a V3 vault or strategy.
+     * @dev This returns the expected APR based off the current
+     * rate of profit unlocking for either a vault or strategy.
+     *
+     * Will return 0 if there is no profit unlocking or no assets.
+     *
+     * @param _vault The address of the vault or strategy.
+     * @return _apr The expected current apr expressed as 1e18.
+     */
+    function getVaultApr(address _vault) external view returns (uint256 _apr) {
+        IVault vault = IVault(_vault);
+
+        uint256 assets = vault.totalAssets();
+
+        if (assets == 0) return 0;
+
+        uint256 unlockingRate = vault.profitUnlockingRate();
+
+        _apr =
+            (1e18 * unlockingRate * SECONDS_PER_YEAR) /
+            MAX_BPS_EXTENDED /
+            assets;
+    }
 }
